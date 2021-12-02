c325f4667bce98309188ee9a38f33394c5034196
YihuiTham <yihuitham@gmail.com>
2021-12-02 21:45:28 +0800

    budgtr app

---
+++ server.js
@@ -0,0 +1,39 @@
+//////dependencies//////
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+//////database//////
+const budgets = require('./models/budget');
+
+//////body parser//////
+app.use(express.urlencoded());
+
+//////routes//////
+//index route
+app.get('/budgets', (req, res) => {
+  const balance = budgets.reduce((prev, cur) => prev + cur.amount, 0);
+  res.render('index.ejs', { balance: balance, budgets: budgets });
+});
+
+//new route
+app.get('/budgets/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+//show route
+app.get('/budgets/:index', (req, res) => {
+  const { index } = req.params;
+  res.render('show.ejs', { budget: budgets[index] });
+});
+
+//create route
+app.post('/budgets/new', (req, res) => {
+  budgets.push(req.body);
+  res.redirect('/budgets');
+});
+
+//////listener//////
+app.listen(PORT, () => {
+  console.log('Budget app listening on port ', PORT);
+});
